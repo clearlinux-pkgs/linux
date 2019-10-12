2ad02f33bb1cdd476e7b4e12275bea53d640ebe7
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2019-10-11 20:58:43 -0500

    introduce kdevelop makefile target
    "make kdevelop" is a new target to download and extract the
    vanilla kernel sources and apply on top of it the Clear Linux
    patches.
    
    You can use DESTDIR=target to specify the directory to extract the
    sources. i.e. "make kdevelop DESTDIR=/tmp"

---
+++ scripts/develop.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+SPECFILE=$1
+DESTDIR=$2
+
+for c in grep cmp sha1sum make tar git
+do
+    if ! command -v $c > /dev/null 
+    then
+        echo >&2 "The script needs the \"$c\" command, and it was not found."
+        exit 1
+    fi
+done
+
+if [ ! ${SPECFILE} ]
+then
+    echo >&2 "${SPECFILE} not found"
+    exit 1
+fi
+
+if [ -z "${DESTDIR}" ]
+then
+    DESTDIR=.
+fi
+
+SRC_URL=$(grep "^Source0:" "${SPECFILE}" | cut -f 2- -d ':' | tr -d " ")
+SRC_FILE=${SRC_URL##*/}
+SRC_DIR=${SRC_FILE%*.tar.xz}
+SRC_VER=${SRC_DIR#*-}
+
+if [ ! -f ${SRC_FILE} ]
+then
+    # Get upstream sources
+    make sources
+fi
+
+echo $(sha1sum ${SRC_FILE} | cut -d\  -f1)/${SRC_FILE} > upstream.check
+
+if ! cmp --quiet upstream upstream.check 
+then
+    echo >&2 "${SRC_FILE} checksum fails"
+    rm upstream.check
+    exit 2
+fi
+
+rm upstream.check
+
+rm -rf ${DESTDIR}/${SRC_DIR}
+tar xf ${SRC_FILE} -C ${DESTDIR}
+
+git -C ${DESTDIR}/${SRC_DIR} init --quiet
+git -C ${DESTDIR}/${SRC_DIR} config gc.auto 0
+git -C ${DESTDIR}/${SRC_DIR} add --all
+git -C ${DESTDIR}/${SRC_DIR} commit -m "${PKG_NAME} ${SRC_VER}" --quiet
+git -C ${DESTDIR}/${SRC_DIR} tag -a -m "v${SRC_VER}" "v${SRC_VER}"
+
+for p in CVE* [0-9]*.patch 
+do
+    git -C ${DESTDIR}/${SRC_DIR} am --quiet $(realpath $p)
+done
+
+cp config ${DESTDIR}/${SRC_DIR}/.config
+
+echo 
+echo "The linux source plus Clear Linux patches is"
+echo "placed at: \"$(realpath ${DESTDIR}/${SRC_DIR})\""
