cf0ff0567788a89605fa2869d8fd536c29aa5f51
Arjan van de Ven <arjan.van.de.ven@intel.com>
2022-07-26 13:36:42 +0000

    version bump from 5.18.14-1160 to 5.18.14-1161

--- linux.spec
+++ linux.spec
@@ -6,7 +6,7 @@
 
 Name:           linux
 Version:        5.18.14
-Release:        1160
+Release:        1161
 License:        GPL-2.0
 Summary:        The Linux kernel
 Url:            http://www.kernel.org/
--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ regexp=`python3 parseversion.py`
 #echo "Regexp is $regexp"
 sed -i -e "$regexp" *.spec
 foo="0"
-git diff || foo="1"
+git diff --exit-code || export foo="1"
+echo $foo
 if [ "$foo" -eq "1" ]; then
 	git commit *.spec
 	make bump
