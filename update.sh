0227f6a16aacd945d03ad2a27ab1723a358843c6
Patrick McCarty <patrick.mccarty@intel.com>
2022-07-29 23:40:42 +0000

    Fix shellcheck warnings; use `git diff --quiet`
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- update.sh
+++ update.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 rm releases.json
 curl -sSf -O -L https://www.kernel.org/releases.json || exit
-regexp=`python3 parseversion.py`
+regexp=$(python3 parseversion.py)
 #echo "Regexp is $regexp"
-sed -i -e "$regexp" *.spec
-foo="0"
-git diff --exit-code || export foo="1"
-echo $foo
-if [ "$foo" -eq "1" ]; then
+sed -i -e "$regexp" ./*.spec
+if ! git diff --quiet ./*.spec; then
 	make generateupstream
-	git add *.spec upstream
-	git commit -m "stable update" *.spec upstream
+	git add ./*.spec upstream
+	git commit -m "stable update" ./*.spec upstream
 	make bump
 	make koji-nowait
 fi
